ae405881ea15c88f3088fd50ac4fc43605c9039e
blck-snwmn <whitesnowmancreator@gmail.com>
2019-09-04 01:32:05 +0900

    Add mock RequestNotification test

--- mock_processor/prcs_mock_test.go
+++ mock_processor/prcs_mock_test.go
@@ -34,3 +34,44 @@ func testRegisterProcess(t *testing.T, client pb.ProcessorClient) {
 		t.Errorf("mocking failed")
 	}
 }
+
+var nof1 = &pb.Notification{Message: "message1"}
+var nof2 = &pb.Notification{Message: "message2"}
+
+func TestRequestNotification(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	stream := prcs.NewMockProcessor_RequestNotificationClient(ctrl)
+	stream.EXPECT().Recv().Return(nof1, nil)
+	stream.EXPECT().Recv().Return(nof2, nil)
+
+	client := prcs.NewMockProcessorClient(ctrl)
+
+	notif := &pb.NotificationRequest{}
+
+	client.EXPECT().RequestNotification(
+		gomock.Any(),
+		notif,
+	).Return(stream, nil)
+
+	testRequestNotification(t, client)
+}
+
+func testRequestNotification(t *testing.T, client pb.ProcessorClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := client.RequestNotification(ctx, &pb.NotificationRequest{})
+	if err != nil {
+		t.Errorf("failed err=%v", err)
+	}
+	for _, nof := range []*pb.Notification{nof1, nof2} {
+		got, err := stream.Recv()
+		if err != nil {
+			t.Errorf("failed to Recv. err=%v", err)
+		}
+		if got.Message != nof.Message {
+			t.Errorf("failed to unmatch Recv. actual %v. want %v", got.Message, nof.Message)
+		}
+	}
+}
